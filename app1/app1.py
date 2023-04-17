9dd7840b2aebdf15c15ad6ef5a68b563a0d1a6f0
Alex Wendel <alexwendel11@gmail.com>
2023-04-17 15:57:12 -0300

    Modifications

--- app1/app1.py
+++ app1/app1.py
@@ -5,12 +5,10 @@ import json
 app1 = Flask(__name__)
 
 
-@app1.route('/')
+@app1.route("/")
 def hello_world():
-    return 'Salam alikom, this is App1 :) '
+    return """<html><head><title>Gawr Gura</title></head><body><h1>Gawr Gura</h1><img src="https://dotesports.com/wp-content/uploads/2023/02/02001438/Gawr-Gura-Return-to-Streaming.png?w=1200"></body></html>"""
 
 
-if __name__ == '__main__':
-   app1.run(debug=True, host='0.0.0.0')
-
-
+if __name__ == "__main__":
+    app1.run(debug=True, host="0.0.0.0")
--- app2/app2.py
+++ app2/app2.py
@@ -4,10 +4,10 @@ import json
 app2 = Flask(__name__)
 
 
-@app2.route('/')
+@app2.route("/")
 def hello_world():
-    return ' Salam alikom, this is App2 :)'
+    return """<html><head><title>Amelia Watson</title></head><body><h1>Amelia Watson</h1><img src="https://yt3.googleusercontent.com/RZ4Fp3L6_zyq6YA7s5WnH8-22iezMLwdJhtkBgs_EAb06mvMCnF59JKMNu9YPCqb7nhaeXMdPvY=s900-c-k-c0x00ffffff-no-rj"></body></html>"""
 
 
-if __name__ == '__main__':
-   app2.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app2.run(debug=True, host="0.0.0.0")
